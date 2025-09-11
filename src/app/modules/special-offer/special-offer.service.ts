ee12b6bc8a2e2e93538e44fe400ef6b2304ac1a5
mohammodrubel <md.rubel007@hotmail.com>
2025-09-11 13:44:22 +0600

    special-offer-service complete

--- src/app/modules/special-offer/special-offer.service.ts
+++ src/app/modules/special-offer/special-offer.service.ts
@@ -4,33 +4,61 @@ import httpStatus from "http-status";
 import { sendImageCloudinary } from "../../utils/sendImageToCloudinary";
 import prisma from "../../utils/prisma";
 
-const crateSpecialOffer = async (file: Express.Multer.File,  data:SpecialOffer) => {
-    if (!file) throw new AppError(httpStatus.CONFLICT, 'Image is required');
-    const imageName = new Date().toTimeString().replace(/:/g, '-') + '-' + file.originalname;
+const createSpecialOffer = async (file: Express.Multer.File, data: SpecialOffer) => {
+  if (!file) throw new AppError(httpStatus.CONFLICT, "Image is required");
 
-    const uploadResult: any = await sendImageCloudinary(file.buffer, imageName);
+  const imageName = new Date().toTimeString().replace(/:/g, "-") + "-" + file.originalname;
+  const uploadResult = await sendImageCloudinary(file.buffer, imageName);
 
-    
-    const result = await prisma.specialOffer.create({
-      data: {
-        ...data,
-        image: uploadResult.secure_url,
-      },
-    });
+  const result = await prisma.specialOffer.create({
+    data: {
+      ...data,
+      image: uploadResult.secure_url,
+    },
+  });
 
-    return result;
+  return result;
 };
 
-const getAllSpecialOffers = async () => {};
+const getAllSpecialOffers = async () => {
+  return await prisma.specialOffer.findMany();
+};
 
-const getSingleSpecialOffer = async (id: string) => {};
+const getSingleSpecialOffer = async (id: string) => {
+  const result = await prisma.specialOffer.findUnique({ where: { id } });
+  if (!result) throw new AppError(httpStatus.NOT_FOUND, "This Special offer is not found");
+  return result;
+};
 
-const editSpecialOffer = async (id: string) => {};
+const editSpecialOffer = async (id: string, data: Partial<SpecialOffer>, file?: Express.Multer.File) => {
+  const offer = await prisma.specialOffer.findUnique({ where: { id } });
+  if (!offer) throw new AppError(httpStatus.NOT_FOUND, "This Special offer is not found");
 
-const deleteSpecialOffer = async (id: string) => {};
+  let imageUrl = offer.image;
+  if (file) {
+    const imageName = new Date().toTimeString().replace(/:/g, "-") + "-" + file.originalname;
+    const uploadResult = await sendImageCloudinary(file.buffer, imageName);
+    imageUrl = uploadResult.secure_url;
+  }
+
+  return await prisma.specialOffer.update({
+    where: { id },
+    data: {
+      ...data,
+      image: imageUrl,
+    },
+  });
+};
+
+const deleteSpecialOffer = async (id: string) => {
+  const result = await prisma.specialOffer.findUnique({ where: { id } });
+  if (!result) throw new AppError(httpStatus.NOT_FOUND, "This Special offer is not found");
+
+  return await prisma.specialOffer.delete({ where: { id } });
+};
 
 export const SpecialOfferService = {
-  crateSpecialOffer,
+  createSpecialOffer,
   getAllSpecialOffers,
   getSingleSpecialOffer,
   editSpecialOffer,
